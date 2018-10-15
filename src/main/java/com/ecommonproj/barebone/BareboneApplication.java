c742a23466215cd81ddfcefa177337cfdd637de2
Yahsi, Ercan (CT RDA DS EU TR PLT MSS) <ercan.yahsi@siemens.com>
2018-10-16 00:01:02 +0300

    initial commit

---
+++ src/main/java/com/ecommonproj/barebone/BareboneApplication.java
@@ -0,0 +1,12 @@
+package com.ecommonproj.barebone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BareboneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BareboneApplication.class, args);
+	}
+}
